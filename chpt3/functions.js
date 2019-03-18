46bd7d5f3af1a23e731d63ff65c9129c3230abb8
jgam <jgam@alumni.nd.edu>
2019-03-18 10:37:04 +0900

    history done

--- chpt3/functions.js
+++ chpt3/functions.js
@@ -15,3 +15,33 @@ function min(a, b){
 console.log(min(0,10))//0
 console.log(min(0, -10))//10
 console.log(min(1,1))//numbers are equal
+
+//Recursion
+
+function isEven(target){
+    function find(current, history){
+        if(current == 1){
+            console.log(history);
+            return false;
+        }
+        else if(current == 0){
+            console.log(history);
+            return true;
+        }
+        else if(current < 0){
+            return find(-current, "${current}")
+        }
+        else {
+            return find(current-2, '(${history} - 2)');
+        }
+    }//`(${history} + 5)
+    return find(target, "${target}");
+}
+
+console.log(isEven(24));//even
+console.log(isEven(75));//odd
+console.log(isEven(-1));//odd
+//history to be printed?
+
+
+
