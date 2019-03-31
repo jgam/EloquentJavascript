e89261033d910d8a547815a90eb9c2f75e033fc0
Jimmy Gam <jimmygam@Jimmyui-MacBook-Pro.local>
2019-03-31 22:10:52 +0900

    first done

--- yan/technical1.js
+++ yan/technical1.js
@@ -10,12 +10,26 @@ Specifically, the callback function is provided 3 arguments:
 3) the collection itself
 */
 
+function callbackFunction(value, index, collection){
+    if (collection[index] == value){
+        console.log("valid")
+        //is array mutable?
+    }
+}
+
 function each(array, callbackFunction) {
-    for(let i = 0; i    callbackFunction(array[i], i, array);
+    for(let i =0; i< Object.entries(array).length; i++){
+        callbackFunction(Object.entries(array)[i])
     }
   }
   
-  each([1, 2, 3], console.log);
-  each({a: 1, b: 2, c: 3}, console.log);
-  
-  // TODO: Make this work for objects!
+each([1, 2, 3], console.log);
+each({a: 1, b: 2, c: 3}, console.log);
+/*
+console.log(typeof({a:1, b:2, c:3}))
+v = {a:1, b:2, c:3}
+console.log(v)
+console.log(Object.entries(v))
+console.log(Object.entries(v).length)
+*/
+// TODO: Make this work for objects!
