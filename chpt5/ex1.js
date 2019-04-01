b8052ca41dc8d47c36b340e99eb0dba6c60c289a
jgam <jgam@alumni.nd.edu>
2019-04-01 11:22:35 +0900

    done

--- chpt5/ex1.js
+++ chpt5/ex1.js
@@ -15,12 +15,29 @@ console.log(labels);
 
 function noisy(f){
     return (...args) => {
-        console.log("calling with ", args);
+        console.log("calling with ", args, typeof(args), typeof(args[4]),typeof(args[1]));
         let result = f(...args);
         console.log("called with ", args, ", returned", result);
         return result;
     }
 }
+let a = "first";
+let b = "second";
 
-noisy(Math.min)(3,2,1);//calling with [3,2,1]
+noisy(Math.min)(a,b,3,2,1);//calling with [3,2,1]
 //called with [3,2,1], returned 1
+//NAN because we can't get a minimum out of object with strings!
+
+//functions to provide new types of control flow
+
+function unless(test, then){
+    if (!test) then();
+}
+
+repeat(3, n=> {
+    unless(n%2 == 1, new_func =>{
+        console.log(n, "is even");
+    });
+});
+// -> 0 is even
+// -> 2 is even
