9f1c165b854ceac1751c7881cf87919630771c46
jgam <jgam@alumni.nd.edu>
2019-04-01 11:04:22 +0900

    exercise

--- chpt5/ex1.js
+++ chpt5/ex1.js
@@ -11,3 +11,16 @@ repeat(5, i => {
 });
 console.log(labels);
 
+//functions that change other functions
+
+function noisy(f){
+    return (...args) => {
+        console.log("calling with ", args);
+        let result = f(...args);
+        console.log("called with ", args, ", returned", result);
+        return result;
+    }
+}
+
+noisy(Math.min)(3,2,1);//calling with [3,2,1]
+//called with [3,2,1], returned 1
