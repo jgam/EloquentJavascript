f34c3940db397f62651eeabb989e0076c5b288ab
jgam <jgam@alumni.nd.edu>
2019-04-05 15:21:04 +0900

    deshatizeit done

--- codewars/6kyu/deshatizeit.js
+++ codewars/6kyu/deshatizeit.js
@@ -1,19 +1,29 @@
 function dashatize(num) {
     //get 'em
+
+    //convert to a string
     str = num.toString();
+
+    //exception 1
     if (str=="NaN"){
       return str;
     }
     
+    //exception 2
     if (parseInt(str) < 0){
       str = str.substring(1);
     }
     
+    //declare return string
     let ret_str = "";
+
+    //actual logic
     for(let i = 0; i < str.length; i++){
+    //even number just move on
       if(parseInt(str[i]) % 2 == 0){
         ret_str += str[i];
       }
+    //odd number needs to be taken care of
       else{
         if(ret_str[ret_str.length-1] == "-"){
           ret_str += str[i]+"-";
