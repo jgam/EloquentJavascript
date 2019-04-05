c2c3e761d5790cb8a01715741ddbfeb3ce6e8d8d
jgam <jgam@alumni.nd.edu>
2019-04-05 12:58:10 +0900

    start of 7 kyu

---
+++ chpt5/example1.js
@@ -0,0 +1,11 @@
+function wrapValue(n) {
+    let local = n;
+    return () => local;
+  }
+  
+  let wrap1 = wrapValue(1);
+  let wrap2 = wrapValue(2);
+  console.log(wrap1());
+  // → 1
+  console.log(wrap2());
+  // → 2
