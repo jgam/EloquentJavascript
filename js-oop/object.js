d804fff0289846cefb5fcfa5c9611a0c420ea08a
jgam <jgam@alumni.nd.edu>
2019-04-15 15:58:49 +0900

    great

--- js-oop/object.js
+++ js-oop/object.js
@@ -1,1 +1,27 @@
 var memberArray = ['egoing', 'graphittie', 'leezhce'];
+
+var MyMath = {
+    PI: Math.PI,
+    random: function(){
+        return Math.random()
+    },
+    floor:function(val){
+        return Math.floor(val);
+    }
+}
+
+console.log("MyMath.PI", MyMath.PI);
+console.log("MyMath.random()", MyMath.random());
+console.log("MyMath.floor(3.9)", MyMath.floor(3.9));
+
+// the use of "This" in obeject
+var kim = {
+    name: 'Kim',
+    first: 10,
+    second: 20,
+    sum:function(f,s){
+        return f+s;
+    }
+}
+
+console.log(kim.sum(kim.first, kim.second));//=> 30
