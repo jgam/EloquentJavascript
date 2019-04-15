fbab22a1043459b032519528a71d10c1722f2dd9
jgam <jgam@alumni.nd.edu>
2019-04-15 13:53:13 +0900

    done

--- codewars/5kyu/convertFrac.js
+++ codewars/5kyu/convertFrac.js
@@ -2,6 +2,8 @@ function convertFrac(lst){
     //Your code here
     // get the leat common factor => euclidean algorithm
     // get the factors of each and multiply it to numerator
+    
+    //get the LCM
     function get_LCM(a, b){
       let temp = 0;
       let large = Math.max(a,b);
@@ -16,6 +18,7 @@ function convertFrac(lst){
       return (a*b/gcd);
     }
     
+    //Declaring variables
     let LCM = 1
     let denom_list = [];
     let numer_list = [];
@@ -26,20 +29,14 @@ function convertFrac(lst){
       denom_list.push(lst[i][1]);
     }
     
-    console.log(LCM)
     let factor_list = [];
-    
-    for(let i =0; i<denom_list.length; i++){
-      factor_list.push(LCM / denom_list[i]);
-    }
-    
     let ret_list = [];
     let numer = 0
     let ret_str = '(';
     
-    for(let i = 0; i < factor_list.length; i++){
-      numer = factor_list[i] * numer_list[i];
-      console.log(numer);
+    for(let i =0; i<denom_list.length; i++){
+      //factor_list.push(LCM / denom_list[i]);
+      numer = LCM / denom_list[i] * numer_list[i]
       ret_str += numer.toString()+','+LCM.toString()+')(';
     }
     ret_str = ret_str.substring(0, ret_str.length-1);
